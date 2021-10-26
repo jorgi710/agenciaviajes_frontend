fcb338ec0684b6177f9723d558df5ce433a80c0d
DS Andres Ayalagit <jorgi710@hotmail.com>
2021-10-26 08:10:30 -0500

    Primer commit Andres

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      <div id="app" class="app">
+            <div class="header">
+                  <h1>Banco Misión TIC</h1>
+            </div>
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+      name: 'App',
+      data: function(){
+      },
+      methods:{
+      },
+      created: function(){
+      }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+      <style>
+      body{
+            margin: 0 0 0 0;
+      }
+      .header{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 10vh;
+            min-height: 100px;
+            background-color: #283747 ;
+            color:#E5E7E9 ;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+      }
+      .header h1{
+            width: 20%;
+            text-align: center;
+      }
 </style>
