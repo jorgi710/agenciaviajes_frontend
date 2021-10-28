e9174d7c94f5eca3c44ff5b8087f7bb5a510bc95
DS Andres Ayalagit <jorgi710@hotmail.com>
2021-10-28 08:39:32 -0500

    Correccion del router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
       <div id="app" class="app">
             <div class="header">
-                  <h1>Banco Misión TIC</h1>
+                  <h1>AGENCIA DE VIAJES</h1>
+                  <nav>
+                        <button v-if="is_auth" > Inicio </button>
+                        <button v-if="is_auth" > Cuenta </button>
+                        <button v-if="is_auth" > Cerrar Sesión </button>
+                        <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+                        <button v-if="!is_auth" v-on:click="loadSignUp" > Registrarse </button>
+                  </nav>
+            </div>
+            <div class="main-component">
+                  <router-view
+                          v-on:completedLogIn="completedLogIn"
+                          v-on:completedSignUp="completedSignUp"
+                  >
+                  </router-view>
+            </div>
+            <div class="footer">
+                  <h2>Misión TIC 2022 - GRUPO 2</h2>
             </div>
       </div>
 </template>
 
-<script>
-export default {
-      name: 'App',
-      data: function(){
-      },
-      methods:{
-      },
-      created: function(){
-      }
-}
-</script>
-      <style>
+<style>
       body{
             margin: 0 0 0 0;
       }
       .header{
-            margin: 0;
+            margin: 0%;
             padding: 0;
             width: 100%;
             height: 10vh;
@@ -36,5 +42,47 @@ export default {
       .header h1{
             width: 20%;
             text-align: center;
+      }
+      .header nav {
+            height: 100%;
+            width: 20%;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            font-size: 20px;
+      }
+      .header nav button{
+            color: #E5E7E9;
+            background: #283747;
+            border: 1px solid #E5E7E9;
+            border-radius: 5px;
+            padding: 10px 20px;
+      }
+      .header nav button:hover{
+            color: #283747;
+            background: #E5E7E9;
+            border: 1px solid #E5E7E9;
+      }
+      .main-component{
+            height: 75vh;
+            margin: 0%;
+            padding: 0%;
+            background: #FDFEFE ;
+      }
+      .footer{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 10vh;
+            min-height: 100px;
+            background-color: #283747;
+            color: #E5E7E9;
+      }
+      .footer h2{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
       }
 </style>
